20ef53bd73387929d451dc6955622fcf4285bbc4
justin <justinwoo97@gmail.com>
2020-12-29 23:06:04 -0500

    personal change

--- static/js/home.js
+++ static/js/home.js
@@ -3,7 +3,7 @@ $(function() {
     strings: [
       "stat justin.human<br/>" + 
       "><span class='caret'>$</span> skills: C++, Python, PostgreSQL, HTML, CSS, JavaScript<br/> ^100" +
-      "><span class='caret'>$</span> projects:<a href='https://github.com/justinwoo97/portfolio_website'>Persoanl Website</a>, <a href='https://github.com/sai-cs-stud/NJLegisAnalysis'>NJ gerrymandering</a><br/> ^100" +
+      "><span class='caret'>$</span> projects:<a href='https://github.com/justinwoo97/portfolio_website'>Personal Website</a>, <a href='https://github.com/sai-cs-stud/NJLegisAnalysis'>NJ gerrymandering</a><br/> ^100" +
       "><span class='caret'>$</span> hobbies: football (Patrick Mahomes == Goat), travel <br/> ^300" +
       "><span class='caret'>$</span> current work:  <a href='https://github.com/shantanu/smart-box'>maestro-smartbox</a><br/>"
     ],
